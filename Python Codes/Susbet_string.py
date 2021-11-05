f4b996547e2051c83a989888786f8b60aa76ff88
pranavsai0809 <pranav.sai0809@gmail.com>
2021-11-05 09:56:51 +0530

    Documentation was written for Subset_string.py

--- Python Codes/Susbet_string.py
+++ Python Codes/Susbet_string.py
@@ -1,15 +1,19 @@
 def subset(ip,op=''):
-    pass
+    '''Args : ip -> input string whose subsequences are to be calculated
+              op -> output string (default is Null) , substrings are included/excluded into the output string 
+              
+       Returns : None , the function only prints the output and returns None'''
 
     if len(ip) == 0:
+        # Base Condition , print the output conmdition when there are no more charecters to add
         print(op,end = ' ')
         return
 
     
     else :
-        
-        subset(ip[1:],op+ip[0])
-        subset(ip[1:],op)
+        # Recursive / iterative condition
+        subset(ip[1:],op+ip[0]) # inclusion of a string charecter into the output 
+        subset(ip[1:],op) # exclusion of string charecter into the output
 
-
+# Driver Code
 subset('abc')
